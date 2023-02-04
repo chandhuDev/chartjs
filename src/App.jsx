eea5190499e88d0f7e2b66a18a774bb7a0df21be
chandhu <chandhudev0@gmail.com>
2023-02-04 21:34:02 +0530

    completed the default dynamic chartjs features

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react'
-import Chart from './chart'
-
-
+import Chart from './Chart'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputData, setInputData] = useState([])
+  const [sortData,setSortData]=useState([])
 
+  function submitData(){
+     const sortedData=inputData.sort(function (a,b){
+      return a-b
+     })
+     setInputData([])
+     setSortData(sortedData)
+     
+  }
+
+  function handleChange(e){
+    setInputData(e.target.value.split(','))
+    
+  }
 
   return (
     <>
@@ -14,12 +26,14 @@ function App() {
       <input 
       className="bg-white border focus:outline focus:outline-offset-2 focus:outline-pink-500 :bg-slate-700 border-gray-400 rounded-md  py-2 px-4 block w-2/6  leading-tight focus:outline-none hover:shadow-outline-blue focus:shadow-outline-blue transition duration-150 ease-in-out" 
       type="text" 
-      placeholder="Enter number"
+      placeholder="Enter values"
+      value={inputData}
+      onChange={handleChange}
       />
-      <button className='transition px-4 py-2 rounded-lg ease-in-out delay-50 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300'>Enter</button>
+      <button onClick={submitData} className='transition px-4 py-2 rounded-lg ease-in-out delay-50 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300'>Enter</button>
       </div>  
-      <div className='w-3/5 h-3/5 rounded-md bg-pink-400 mt-14 shadow-xl transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300'>
-        <Chart />
+      <div className='w-3/5 h-3/5 rounded-md bg-white mt-14 shadow-xl cursor-pointer'>
+        <Chart Data={sortData} />
       </div>
       </div>
     </>
--- src/Chart.jsx
+++ src/Chart.jsx
@@ -1,13 +1,65 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,} from "chart.js";
 
-const Chart = () => {
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Legend,
+    Tooltip
+  );
 
 
+
+  export const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart',
+      },
+    },
+  };
+
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+ 
+
+const Chart = ({Data}) => {
     
+    
+
+    const data = {
+      labels,
+      datasets: [
+        {
+          label: 'Dataset',
+          data: Data,
+          backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        },
+      ],
+    };
+
+
+
   return (
-    <>
-      
-    </>
+   <>
+   <div className='w-full h-full '>
+   <Bar options={options} data={data} />;
+   </div>
+   </>
   )
 }
 
