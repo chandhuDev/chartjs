1c4d8eb65ffe97a50a59bb5535c598054164dd63
chandhu <chandhudev0@gmail.com>
2023-07-14 11:44:32 +0530

    fixes the UI to responsive

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import { Chart as ChartJS,
   BarElement,
   Title,
   Tooltip,
-  Legend,} from "chart.js";
+  Legend
+} from "chart.js";
 
   ChartJS.register(
     CategoryScale,
@@ -67,9 +68,7 @@ function App() {
      if(e.key==="Enter"){
        setData(inputValue)
        setInputData('')
-       
       }
-      
   }
 
   function setData(Data){
@@ -98,23 +97,19 @@ function App() {
 
   function handleChange(e){
     setInputData(e.target.value)
-    
     if(e.target.value.split(',').length>1){
       setInputValue(e.target.value.split(','))
       setLabelData(generateArray(e.target.value.split(',').length))
-    }else{
+    } else{
       setInputValue([...inputValue,e.target.value.split(',')])
       setLabelData([...labelData,count++])
     }
-    
   }
 
- 
-
-   return (
+  return (
     <>
-      <div className='flex w-full h-screen  flex-col justify-start   items-center'>
-      <div className='flex flex-row w-2/5 gap-x-5 mt-12 justify-center'>
+      <div className='flex w-full h-screen bg-indigo-600/80 flex-col justify-start items-center'>
+      <div className='flex flex-row md:w-2/4 w-full gap-x-5 mt-12 justify-center'>
       <input 
       className="bg-white border focus:outline focus:outline-offset-2 focus:outline-pink-500 :bg-slate-700 border-gray-400 rounded-md  py-2 px-4 block w-2/6  leading-tight focus:outline-none hover:shadow-outline-blue focus:shadow-outline-blue transition duration-150 ease-in-out" 
       type="text" 
@@ -123,9 +118,9 @@ function App() {
       onChange={handleChange}
       onKeyUp={submitData}
       />
-      <button onClick={sort} className='transition px-4 py-2 rounded-lg ease-in-out delay-50 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300'>Sort</button>
+      <button onClick={sort} className='transition px-4 py-2 text-white font-semibold rounded-lg ease-in-out delay-50 bg-gray-700/80 hover:-translate-y-1 hover:scale-110 hover:bg-gray-700/80 duration-300'>Sort</button>
       </div>  
-      <div className='w-3/5 h-3/5 rounded-md bg-white mt-14 shadow-xl cursor-pointer'>
+      <div className='md:w-3/5 md:h-3/5 w-ful h-auto rounded-md bg-white mt-14 shadow-xl cursor-pointer'>
       <Bar options={options} data={sortData} redraw={true}/>;
       </div>
       </div>
