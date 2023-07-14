e4906607d9d3100c20b3913d642e2feedb6c7e36
chandhu <chandhudev0@gmail.com>
2023-07-14 11:50:20 +0530

    fixes the small screen size responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -108,19 +108,19 @@ function App() {
 
   return (
     <>
-      <div className='flex w-full h-screen bg-indigo-600/80 flex-col justify-start items-center'>
+      <div className='flex w-full h-screen flex-col justify-start items-center'>
       <div className='flex flex-row md:w-2/4 w-full gap-x-5 mt-12 justify-center'>
       <input 
-      className="bg-white border focus:outline focus:outline-offset-2 focus:outline-pink-500 :bg-slate-700 border-gray-400 rounded-md  py-2 px-4 block w-2/6  leading-tight focus:outline-none hover:shadow-outline-blue focus:shadow-outline-blue transition duration-150 ease-in-out" 
+      className="bg-white border focus:outline focus:outline-offset-2 focus:outline-pink-900 :bg-slate-700 border-gray-800 rounded-md  py-2 px-4 block w-2/6  leading-tight focus:outline-none hover:shadow-outline-blue focus:shadow-outline-blue transition duration-150 ease-in-out" 
       type="text" 
       placeholder="Enter values"
       value={inputData}
       onChange={handleChange}
       onKeyUp={submitData}
       />
-      <button onClick={sort} className='transition px-4 py-2 text-white font-semibold rounded-lg ease-in-out delay-50 bg-gray-700/80 hover:-translate-y-1 hover:scale-110 hover:bg-gray-700/80 duration-300'>Sort</button>
+      <button onClick={sort} className='transition px-4 py-2 font-semibold text-white rounded-lg ease-in-out delay-50 bg-gray-700/80 hover:-translate-y-1 hover:scale-110 hover:bg-gray-700/80 duration-300'>Sort</button>
       </div>  
-      <div className='md:w-3/5 md:h-3/5 w-ful h-auto rounded-md bg-white mt-14 shadow-xl cursor-pointer'>
+      <div className='md:w-3/5 md:h-3/5 w-full h-auto rounded-md bg-white mt-14 shadow-xl cursor-pointer'>
       <Bar options={options} data={sortData} redraw={true}/>;
       </div>
       </div>
